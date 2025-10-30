7ff3fff761db8f857e657d2c5e5d6ec57c69d9d8
SorayaGomesS <sorayagomes62@gmail.com>
2025-10-30 16:56:52 -0300

    Create forms.py

---
+++ app/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    email = BooleanField('Deseja enviar e-mail para flaskaulasweb@zohomail.com?')
+    submit = SubmitField('Submit')
